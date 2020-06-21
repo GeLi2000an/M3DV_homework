40587e12e718297bbda0edfd3f58641f794f7986
GeLi2000an <48593160+GeLi2000an@users.noreply.github.com>
2020-06-21 23:23:27 +0800

    Add files via upload

---
+++ mix_up.py
@@ -0,0 +1,20 @@
+def mixup_data(x, y, alpha=1.0):
+    '''Returns mixed inputs, pairs of targets, and lambda'''
+    if alpha > 0:
+        lam = np.random.beta(alpha, alpha)
+    else:
+        lam = 1
+
+    batch_size = x.size()[0]
+    index = torch.randperm(batch_size)
+
+    mixed_x = lam * x + (1 - lam) * x[index, :]
+    y_a, y_b = y, y[index]
+    return mixed_x, y_a, y_b, lam
+
+"""
+交叉熵第二个参数为int型的，不能直接输入
+为了使用cross_entropy loss, 需要重新写一个损失函数
+"""
+def mixup_criterion(criterion, pred, y_a, y_b, lam):
+    return lam * criterion(pred, y_a) + (1 - lam) * criterion(pred, y_b)
